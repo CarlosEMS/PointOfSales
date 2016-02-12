2995041ffb6e3ba100abc4ac100d7fde365ca1ea
Ahmed Nasr <ahmednasr@anasr.net>
2016-02-12 02:23:22 +0100

    Update POSModule.py

--- POSModule.py
+++ POSModule.py
@@ -1,57 +1,41 @@
 # -*- coding: utf-8 -*-
 """
 Here will be all the code related to ou data, Add, Update, Delete, and Get
-"""
+""" 
 
 #Dictionary of Articles
 Articles={
-          1: ("Book: Lean Analytics",100),
-          2: ("Book: Lean Start-Up",200),
-          3: ("Book: Data Mining",300),
-          4: ("Book: Why Nations Fail",400),
-          5: ("Book: Getting to Yes",500),
-          6: ("Music: Passion Pit",600)
+          "A00000001": ("Book: Lean Analytics"),
+          "A00000002": ("Book: Lean Start-Up"),
+          "A00000003": ("Book: Data Mining"),
+          "A00000004": ("Book: Why Nations Fail"),
+          "A00000005": ("Book: Getting to Yes"),
+          "A00000006": ("Music: Passion Pit")
           }
 
 #Dictionary of customers
-Customers={1: "Rahul",
-           2: "Xuan",
-           3: "Carlos",
-           4: "Ahmed",
-           5: "Gaille"}
+Customers={"C00000001": "Rahul",
+           "C00000002": "Xuan",
+           "C00000003": "Carlos",
+           "C00000004": "Ahmed",
+           "C00000005": "Gaille"}
 
-#List of Lists of sales (ArticleID, CustomerID, Quantity, Cash or Credit, Payment)
+#List of Lists of sales (ArticleID, CustomerID, Cash or Credit (zero or One), Payment)
 Sales=[
-    [1,1,10,"cash",1000],
-    [1,2,10,"credit",2000],
-    [2,1,10,"cash",1000],
-    [2,1,10,"credit",1000],
-    [2,3,10,"cash",3000],
-    [2,3,10,"credit",3000]
+    ["A00000003","C00000001",0,1000],
+    ["A00000006",0,1,2000],
+    ["A00000005","C00000001",0,3000],
+    ["A00000005","C00000002",1,2000],
+    [0,0,1,1000],
+    [0,0,0,3000],
         ]
-
-
-#List with allowed payment methods
-PaymentMethod=["cash","credit"]
+        
+#Last30Days
+Last30Days=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
 
 #Choices of inputs to the console that can be done by the user
-InputCommand={"S":"[S] Add a sale",
-         "R":"[R] Generate Report",
-         "CRM":"[CRM] CRM",
-         "D":"[D] Close of Day",
-         "AC":"[AC] Add New Customer",
-         "Q":"[Q] Quit",
-         "AllC":"[AllC] Show All Customers"
-         }
+InputCommand=["sale","customer","report","crm","close day","quit","close"]
 
-
-"""
-Desc: Returns the payment types
-Param:
-Return: List of payment types.
-"""
-def GetPaymentTypesm():
-    return PaymentMethod
 
 """
 Desc: Returns the input commands of the system that can be used by the user
@@ -59,6 +43,9 @@ Param:
 Return: Dictionary of input commands
 """
 def GetInputCommands():
+    '''
+    check doc
+    '''
     return InputCommand
 
 """
@@ -91,28 +78,22 @@ def GetItemByID(ItemID, Items):
 
 """
 Desc: Adds new Sale to the Sales
-Param: 
-    ArticleID: ID of the Article
-    CustomerID: ID of the Customer who bought the Article
-    Quantity: Number of Articles bought
-    Payment: string of the payment type
-    Payment: is the payment done by the customer
+Param: SaleList
 Return: Success string if Operation is done
 """
-def AddSalesm(ArticleID,CustomerID,Quantity,PaymentType,Payment):
-    Sales.append((ArticleID,CustomerID,Quantity,PaymentType,Payment))
+def AddSalesm(SaleList):
+    Sales.append(SaleList)
     return "Success"
     
 
 """
 Desc: Adds new Customer
-Param: Customer Name
-Return: Customer ID
+Param: CustomerList
+Return: Success string if Operation is done
 """
-def AddCustomerm(CustomerName):
-    NewID=len(Customers)+1
-    Customers[NewID]=CustomerName
-    return NewID
+def AddCustomerm(CustomerList):
+    Customers[CustomerList[0]]=CustomerList[1]
+    return "Success"
     
 """
 Desc: Getting All Customers
@@ -138,4 +119,30 @@ Return: All Articles
 """
 def GetAllArticlesm():
     return Articles
+    
+"""
+Desc: Remove from the last 30 days list based on its index location
+Param: 
+Return: Success if operation done
+"""
+def Last30DaysPOP(POPIndex):
+    l=Last30Days.pop(POPIndex)
+    return "Success"
+    
+"""
+Desc: Remove from the last 30 days list based on its index location
+Param: 
+Return: Success if operation done
+"""
+def Last30DaysInsertAtFirstDay(ValueToAdd):
+    Last30Days.insert(0,ValueToAdd)
+    return "Success"
+    
+"""
+Desc: Get Last 30 Days Sales
+Param: 
+Return: Last 30 Days Sales
+"""
+def GetLast3DaysSales():
+    return Last30Days
     
